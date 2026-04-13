858733c172e812d4a096ac3d6aacb6e48f0bfaf9
Armaan <armaan002609@gmail.com>
2026-04-13 21:51:44 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 
 // Google Gemini API Key
 // Get yours at: https://aistudio.google.com/
-define('GEMINI_API_KEY', 'AIzaSyCMMwquK6znIKVl7g3rVxuxyMOw0q5NMew');
+define('GEMINI_API_KEY', 'YOUR_API_KEY');
 
 // Google OAuth2 Configuration
 // Get yours at: https://console.cloud.google.com/
-define('GOOGLE_CLIENT_ID', '993866056016-r9d9tsh9mgnm1m2a3mgjd8i5k8dsvgrc.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID');
 
 // Other configurations can be added here
 ?>
